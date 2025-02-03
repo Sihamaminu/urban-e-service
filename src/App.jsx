7d422daa11feea1012648c5f4997db81e613881d
Fuat17h <Fuat17h@gmail.com>
2025-02-04 01:49:44 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import { Calendar } from "@/components/ui/calendar"
+
+
+
+function App() {
+
+
+
+  return (
+    <>
+     <Calendar
+      mode="single"
+      className="rounded-md border shadow"
+    />
+    </>
+  )
+}
+
+export default App
